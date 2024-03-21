ad06c6edde8fa16e18e5af06ca55d8aa6f100719
benjamincarmelo.sanc <bsanlao@uoc.edu>
2024-03-21 11:40:22 +0000

    Frontend inicial en React

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hola Mundo 2</h1>
+        </div>
+    );
 }
 
 export default App;
