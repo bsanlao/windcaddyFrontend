39aa6295e214acc72254974bcd8c78baf079f760
benjamincarmelo.sanc <bsanlao@uoc.edu>
2024-03-21 21:53:18 +0000

    Datos Conditions v1

--- src/pages/WeatherCondition/index.tsx
+++ src/pages/WeatherCondition/index.tsx
@@ -41,19 +41,12 @@ export default function WeatherCondition() {
                 )}
             </Typography>
             <Grid container spacing={2}>
-                <Grid display="flex" alignItems="end" item xs={8} >
-                    <Typography sx={{
-                        lineHeight: '1'
-                    }} variant="h6">
-                        Listado de usuarios
-                    </Typography>
-                </Grid>
                 <Grid item xs={12}>
                     <TableContainer component={Paper}>
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>Condiciones</TableCell>
+                                    <TableCell><b>Condiciones</b></TableCell>
                                     <TableCell>Material</TableCell>
                                     <TableCell>Valoración</TableCell>
                                 </TableRow>
@@ -62,8 +55,14 @@ export default function WeatherCondition() {
                                 <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                     <TableCell component="th" scope="row">
                                         Spot: {condition?.spot}<br/>
-                                        Altura Oleaje: {condition?.alturaOleaje}<br/>
-                                        Viento (knots): {condition?.velmedia}<br/>
+                                        Altura Oleaje: {condition?.alturaOleaje} m<br/>
+                                        Viento: {condition?.velmedia} knots<br/>
+                                        Dirección del Viento: {condition?.direccionViento} degrees<br/>
+                                        Racha de Viento: {condition?.racha} knots<br/>
+                                        Periodo Medio del Oleaje: {condition?.periodoMedioOleaje} s<br/>
+                                        Periodo Pico del Oleaje: {condition?.periodoPicoOleaje} s<br/>
+                                        Dirección del Oleaje: {condition?.direccionOleaje} degrees<br/>
+                                        Velocidad del Viento: {condition?.velocidadViento} knots<br/>
                                     </TableCell>
                                     <TableCell component="th" scope="row"></TableCell>
                                     <TableCell component="th" scope="row"></TableCell>
